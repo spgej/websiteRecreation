09ccfb7b679462d2600fce5a1284f6cdf15dcba6
Samuel <spage930@gmail.com>
2021-06-11 11:57:43 -0400

    Organize CSS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
-
+//Program for iterating through database to render
+//most recent objects as header on main page
+//WIP
 
 const reverse = array.reverse();
 const n = 5;
 const slicedArray = reverse.slice(0, n);
 
-slicedArray.forEach((obj)=>{
+for (var i = 0; i < 3; i++) {
+  slicedArray.forEach((obj.length[i])=>{
 
-});
+  });
+}
