cea109844f47f18deef351357a4c717cd3bf6c33
Samuel <spage930@gmail.com>
2021-06-15 12:48:26 -0400

    Use EJS to Refactor and make main.ejs dynamic

--- app.js
+++ app.js
@@ -13,15 +13,61 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res)=>{
-  res.render("main");
+mongoose.connect("mongodb://localhost:27017/articleDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const schema = new mongoose.Schema({
+  title: String,
+  body: String,
+  imgtag: String,
+});
 
+const Article = mongoose.model("Article", schema);
 
+const art1 = new Article({
+  title:"Why Mega Man X is Definitely the Best Mega Man Game",
+  body:"lorem ipsum",
+  imgtag: "mma",
+});
+const art2 = new Article({
+  title:"50 Reasons Why You Shouldn't Listen to Buzzfeed Ever in Regards to Anything",
+  body:"lorem ipsum",
+  imgtag: "loz",
+});
+const art3 = new Article({
+  title:"Mass Effect Legendary Edition Review: The Same But Slightly Better",
+  body:"lorem ipsum",
+  imgtag: "me",
+});
+const art4 = new Article({
+  title:"LOTR: The Fellowship of the Ring, Cast Revisited, Again",
+  body:"lorem ipsum",
+  imgtag: "lotr",
+});
 
+const artArray = [art1, art2, art3, art4];
 
-
+app.get("/", (req, res)=>{
+  Article.find({}, (err, articles)=>{
+    if (articles.length === 0) {
+      Article.insertMany(artArray, (err)=>{
+        if (err){
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      });
+        res.redirect("/");
+    } else {
+      const slicedArray = articles.slice(Math.max(articles.length - 4, 0));
+      res.render("main", {
+        slicedArray: slicedArray,
+      });
+    }
+  });
+});
 
 
 
