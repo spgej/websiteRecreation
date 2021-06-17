24b39ad5b4ff5df9dc3386d5d1faaa39c8454e59
Samuel <spage930@gmail.com>
2021-06-17 12:14:09 -0400

    Minor Responsiveness added to Nav

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-const express =  require("express");
+const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
@@ -27,39 +27,39 @@ const schema = new mongoose.Schema({
 const Article = mongoose.model("Article", schema);
 
 const art1 = new Article({
-  title:"Why Mega Man X is Definitely the Best Mega Man Game",
-  body:"lorem ipsum",
+  title: "Why Mega Man X is Definitely the Best Mega Man Game",
+  body: "lorem ipsum",
   imgtag: "mma",
 });
 const art2 = new Article({
-  title:"50 Reasons Why You Shouldn't Listen to Buzzfeed Ever in Regards to Anything",
-  body:"lorem ipsum",
+  title: "50 Reasons Why You Shouldn't Listen to Buzzfeed Ever in Regards to Anything",
+  body: "lorem ipsum",
   imgtag: "loz",
 });
 const art3 = new Article({
-  title:"Mass Effect Legendary Edition Review: The Same But Slightly Better",
-  body:"lorem ipsum",
+  title: "Mass Effect Legendary Edition Review: The Same But Slightly Better",
+  body: "lorem ipsum",
   imgtag: "me",
 });
 const art4 = new Article({
-  title:"LOTR: The Fellowship of the Ring, Cast Revisited, Again",
-  body:"lorem ipsum",
+  title: "LOTR: The Fellowship of the Ring, Cast Revisited, Again",
+  body: "lorem ipsum",
   imgtag: "lotr",
 });
 
 const artArray = [art1, art2, art3, art4];
 
-app.get("/", (req, res)=>{
-  Article.find({}, (err, articles)=>{
+app.get("/", (req, res) => {
+  Article.find({}, (err, articles) => {
     if (articles.length === 0) {
-      Article.insertMany(artArray, (err)=>{
-        if (err){
+      Article.insertMany(artArray, (err) => {
+        if (err) {
           console.log(err);
         } else {
           console.log("Success");
         }
       });
-        res.redirect("/");
+      res.redirect("/");
     } else {
       const slicedArray = articles.slice(Math.max(articles.length - 4, 0));
       res.render("main", {
@@ -71,6 +71,6 @@ app.get("/", (req, res)=>{
 
 
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log("Server initialized on port 3000.");
 });
