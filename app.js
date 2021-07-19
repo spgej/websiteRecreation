35429ac85509dfd36bdd7ba45c9d3951b1f412f1
Samuel <spage930@gmail.com>
2021-07-19 10:47:01 -0400

    Procfile

--- app.js
+++ app.js
@@ -146,6 +146,6 @@ app.post("/articles/:articleId", (req, res) => {
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server initialized on port 3000.");
 });
